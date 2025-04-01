a6bb9503306aaa39483cc186094675a0ee42e4f7
Far Tseng <farrrr@gmail.com>
2025-04-01 18:08:17 +0800

    新增 Sheldon's 安裝腳本模板，分別針對 macOS 和 Linux 系統進行配置，並更新 plugins.toml 檔案以改善快捷鍵設定，移除不必要的剪貼簿操作，提升可讀性與使用便利性。

--- home/dot_local/bin/common/keybindings.zsh
+++ home/dot_local/bin/common/keybindings.zsh
@@ -22,36 +22,3 @@ bindkey "^X^L" insert-last-command-output
 # 全域 alias 展開觸發鍵
 zle -N globalias
 bindkey "^X^a" globalias
-
-# Ctrl-x, Ctrl-k：剪掉整行 + 複製到系統剪貼簿
-# Ctrl-u, Ctrl-k：向前/向後刪除 + 複製至剪貼簿
-# ⌘+W 類似效果
-{{- if eq .chezmoi.os "darwin" }}
-pb-copy-region-as-kill () {
-  zle copy-region-as-kill
-  print -rn $CUTBUFFER | pbcopy
-}
-zle -N pb-copy-region-as-kill
-bindkey -e '\ew' pb-copy-region-as-kill
-
-pb-backward-kill-line () {
-  zle backward-kill-line
-  print -rn $CUTBUFFER | pbcopy
-}
-zle -N pb-backward-kill-line
-bindkey -e '^u' pb-backward-kill-line
-
-pb-kill-line () {
-  zle kill-line
-  print -rn $CUTBUFFER | pbcopy
-}
-zle -N pb-kill-line
-bindkey -e '^k' pb-kill-line
-
-pb-kill-buffer () {
-  zle kill-buffer
-  print -rn $CUTBUFFER | pbcopy
-}
-zle -N pb-kill-buffer
-bindkey -e '^x^k' pb-kill-buffer
-{{- end }}
