9026761e22047cf19a3a4233c6d1e7ad3d401f89
Far Tseng <farrrr@gmail.com>
2025-04-03 12:06:08 +0800

    更新 gcloud 別名，新增 --platform linux/amd64 參數以支援容器執行，提升兼容性。

--- home/dot_local/bin/common/aliases.zsh
+++ home/dot_local/bin/common/aliases.zsh
@@ -20,7 +20,7 @@ else
 fi
 
 # gcloud：以容器方式執行 Google Cloud SDK
-alias gcloud='docker run --rm -it -v "${HOME%/}"/.config/gcloud:/root/.config/gcloud gcr.io/google.com/cloudsdktool/cloud-sdk gcloud'
+alias gcloud='docker run --platform linux/amd64 --rm -it -v "${HOME%/}"/.config/gcloud:/root/.config/gcloud gcr.io/google.com/cloudsdktool/cloud-sdk gcloud'
 
 # Bash LSP：使用容器版 bash-language-server
 alias bash-language-server="docker run --platform linux/amd64 --rm -i ghcr.io/shunk031/bash-language-server:latest"
